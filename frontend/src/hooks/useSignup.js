452ad21f8d334a86ad4cd93d8bf9f5c3b9e92d48
The Chayan <chayanmandal532@gmail.com>
2024-05-16 11:05:58 +0530

    handle error is disable for testing

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -35,21 +35,21 @@ const useSignup = () => {
 };
 export default useSignup;
 
-function handleInputErrors({ fullName, username, password, confirmPassword, gender }) {
-	if (!fullName || !username || !password || !confirmPassword || !gender) {
-		toast.error("Please fill in all fields");
-		return false;
-	}
+// function handleInputErrors({ fullName, username, password, confirmPassword, gender }) {
+// 	if (!fullName || !username || !password || !confirmPassword || !gender) {
+// 		toast.error("Please fill in all fields");
+// 		return false;
+// 	}
 
-	if (password !== confirmPassword) {
-		toast.error("Passwords do not match");
-		return false;
-	}
+// 	if (password !== confirmPassword) {
+// 		toast.error("Passwords do not match");
+// 		return false;
+// 	}
 
-	if (password.length < 6) {
-		toast.error("Password must be at least 6 characters");
-		return false;
-	}
+// 	if (password.length < 6) {
+// 		toast.error("Password must be at least 6 characters");
+// 		return false;
+// 	}
 
-	return true;
-};
+// 	return true;
+// };
